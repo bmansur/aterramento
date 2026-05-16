94801c4ac91130f27426ebaa2272ab73a36a9335
Bruno Rolim Mansur <bruno.mansur@gmail.com>
2026-05-15 22:18:02 -0300

    Update exercicio3.js

--- exercicio3.js
+++ exercicio3.js
@@ -1,1 +1,12 @@
+function calcular() {
+  const comprimento = parseFloat(document.getElementById("comprimento").value);
+  const dc = parseFloat(document.getElementById("dc").value);
+  const largura = parseFloat(document.getElementById("largura").value);
+  const dl = parseFloat(document.getElementById("dl").value);
 
+  const ncp = comprimento/dl + 1;
+  const ncj = largura/dc + 1;
+
+  document.getElementById("ncp").innerText = ncp.toFixed(2);
+  document.getElementById("ncj").innerText = ncj.toFixed(2);
+}
