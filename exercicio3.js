53c44e6ea4d371d7b811324a0dca98b6a9dc6fca
Bruno Rolim Mansur <bruno.mansur@gmail.com>
2026-05-16 22:34:11 -0300

    Update exercicio3.js

--- exercicio3.js
+++ exercicio3.js
@@ -15,15 +15,25 @@ function calcular() {
 
   document.getElementById("ncp").innerText = ncp.toFixed(0);
   document.getElementById("ncj").innerText = ncj.toFixed(0);
+  document.getElementById("lcm").innerText = ncj.toFixed(0);
 
   const kmp = 1/(2*Math.PI)*Math.log(dl**2/(4*Math.PI*(ncp-1)*h*0.01433));
   const kmj = 1/(2*Math.PI)*Math.log(dc**2/(4*Math.PI*(ncj-1)*h*0.01433));
 
+  document.getElementById("kmp").innerText = kmp.toFixed(0);
+  document.getElementById("kmj").innerText = kmj.toFixed(0);
+  
   const ksp = (1/Math.PI)*((1/(2*h))+1/(dl+h)+Math.log(0.655*(ncp-1)-0.328)/dl);
   const ksj = (1/Math.PI)*((1/(2*h))+1/(dc+h)+Math.log(0.655*(ncj-1)-0.328)/dc);
 
+  document.getElementById("ksp").innerText = ksp.toFixed(0);
+  document.getElementById("ksj").innerText = ksj.toFixed(0);
+
   const kip = 0.65 + 0.172 * ncp;
   const kij = 0.65 + 0.172 * ncj;
+
+  document.getElementById("kip").innerText = kip.toFixed(0);
+  document.getElementById("kij").innerText = kij.toFixed(0);
 
   const lc = (kmp*kij*pa*icf*Math.sqrt(tf))/(0.116+0.174*3000);
   const epa = 116+0.7*3000/Math.sqrt(tf);
@@ -36,4 +46,16 @@ function calcular() {
   const itmsb = (1000*ete)/(1000+1.5*p1);
   const itmcb = (1000*ete)/(1000+1.5*(p1+ps));
   const ia = ((1000+1.5*ps)*9*lcm)/(1000*kmp*kip*p1);
+
+  document.getElementById("lc").innerText = lc.toFixed(0);
+  document.getElementById("epa").innerText = epa.toFixed(0);
+  document.getElementById("eper").innerText = eper.toFixed(0);
+  document.getElementById("etm").innerText = etm.toFixed(0);
+  document.getElementById("ete").innerText = ete.toFixed(0);
+  document.getElementById("ich").innerText = ich.toFixed(0);
+  document.getElementById("ipmsb").innerText = ipmsb.toFixed(0);
+  document.getElementById("ipmcb").innerText = ipmcb.toFixed(0);
+  document.getElementById("itmsb").innerText = itmsb.toFixed(0);
+  document.getElementById("itmcb").innerText = itmcb.toFixed(0);
+  document.getElementById("ia").innerText = ia.toFixed(0);
 }
